256cce3f58f5904c1b0be81d1c98ef0ea7f52db8
Abdullah Naseem <abdullahnaseem27@gmail.com>
2025-08-07 04:02:04 +0500

    update import statement and enhance logging configuration

--- worker/main.py
+++ worker/main.py
@@ -1,8 +1,13 @@
 import logging
 from livekit.agents import WorkerOptions, cli
-from .entry import entry_point
+from entry import entry_point
 
-logging.basicConfig(level=logging.INFO)
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+
 
 if __name__ == "__main__":
     cli.run_app(WorkerOptions(entrypoint_fnc=entry_point))
