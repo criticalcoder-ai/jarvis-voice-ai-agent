ce2a690adfc1a39ded069ec38ca4775752282649
Abdullah Naseem <abdullahnaseem27@gmail.com>
2025-08-09 15:29:35 +0500

    fix: add request object in function params

--- backend/app/auth/oauth.py
+++ backend/app/auth/oauth.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from authlib.integrations.starlette_client import OAuth
+from fastapi import HTTPException, Request
 from starlette.config import Config
 
 # Get base directory: backend/
@@ -9,11 +10,16 @@ config = Config(BASE_DIR / ".env")
 oauth = OAuth(config)
 
 oauth.register(
-    name="google",
-    server_metadata_url="https://accounts.google.com/.well-known/openid-configuration",
-    client_kwargs={"scope": "openid email profile"}
+    name='google',
+    server_metadata_url='https://accounts.google.com/.well-known/openid-configuration',
+    client_kwargs={'scope': 'openid email profile'}
 )
 
-async def get_google_user_info(request):
+async def get_google_user_info(request: Request):
     token = await oauth.google.authorize_access_token(request)
-    return token.get("userinfo")
+
+    if 'userinfo' not in token:
+        raise HTTPException(status_code=400, detail="User info not available in token.")
+    
+    user_info = token['userinfo']
+    return user_info
